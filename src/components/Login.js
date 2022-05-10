643ce7cba8fcd7c0fe03a50508b44f349d41dbe2
Nurul Islam <nurulbdpersonal@gmail.com>
2022-05-10 19:38:36 +0600

    final code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,51 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 // import { useNavigate } from 'react-router-dom'; 
 
 
-// const inputStyle = {
-//     width: '100%',
-//     height: '30px',
-//     marginBottom: '10px'
-// }
+const inputStyle = {
+    width: '100%',
+    height: '30px',
+    marginBottom: '10px'
+}
 
 const Login = () => {
-    // const navigate = useNavigate();
-    // const handleLogin = event => {
-    //     event.preventDefault();
-    //     const email = event.target.email.value;
-    //     const password = event.target.password.value;
+    const navigate = useNavigate();
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        // console.log(email, password)
 
-    //     fetch('http://localhost:5000/login', {
-    //         method: 'POST',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify({email, password})
-    //     })
-    //     .then(res => res.json())
-    //     .then(data =>{
-    //         if(data.success){
-    //             localStorage.setItem('accessToken', data.accessToken);
-    //             navigate('/orders');
-    //         }
-    //         console.log(data);
-    //     })
-    // }
+        fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.success){
+                localStorage.setItem('accessToken', data.accessToken);
+                navigate('/orders');
+            }
+            console.log(data);
+        })
+    }
 
     return (
-        <div></div>
-        // <form onSubmit={handleLogin} style={{ width: '50%', margin: '20px auto' }}>
-        //     <input style={inputStyle} type="email" name="email" placeholder='Email' />
-        //     <br />
-        //     <input style={inputStyle} type="password" name="password" placeholder='Password' />
-        //     <br />
-        //     <input type="submit" value="Login" />
-        // </form>
+        
+        <form onSubmit={handleLogin} style={{ width: '50%', margin: '20px auto' }}>
+            <input style={inputStyle} type="email" name="email" placeholder='Email' />
+            <br />
+            <input style={inputStyle} type="password" name="password" placeholder='Password' />
+            <br />
+            <input type="submit" value="Login" />
+        </form>
     );
 };
 
 export default Login;
+
+// 
--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,36 +1,34 @@
 import React, { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Orders = () => {
-    // const navigate = useNavigate();
-    // const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
+    const [orders, setOrders] = useState([]);
 
-    // useEffect( () =>{
-    //     fetch('http://localhost:5000/orders', {
-    //         method: 'GET', 
-    //         headers:{
-    //             authorization: `Bearer ${localStorage.getItem('accessToken')}`
-    //         }
-    //     })
-    //     .then(res => {
-    //         if(res.status === 401 || res.status===403){
-    //             navigate('/login');
-    //         }
-    //         return res.json()
-    //     })
-    //     .then(data =>{
-    //         console.log(data);
-    //         setOrders(data);
-    //     })
-    // }, [])
+    useEffect( () =>{
+        fetch('http://localhost:5000/orders', {
+            method: 'GET', 
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => {
+            if(res.status === 401 || res.status===403){
+                navigate('/login');
+            }
+            return res.json()
+        })
+        .then(data =>{
+            console.log(data);
+            setOrders(data);
+        })
+    }, [])
 
     return (
         <div>
-            <h1>This is Orders page: </h1>
+            <h1>This is Orders page: {orders.length}</h1>
         </div>
     );
 };
 
 export default Orders;
-
-// {orders.length}
